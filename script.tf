8d2db02eedc8f06558fb3830cfa5f85f4b36754c
shyni-ls <107136671+shyni-ls@users.noreply.github.com>
2023-11-09 14:23:47 +0530

    Update script.tf

--- script.tf
+++ script.tf
@@ -1,9 +1,13 @@
 provider "aws" {
+  region     = "us-east-1"
 }
-resource "aws_instance" "Shyni" {
+ 
+ 
+resource "aws_instance" "Shyni-Server" {
   ami           = "ami-05c13eab67c5d8861"
   instance_type = "t2.micro"
   tags = {
-      Name = "Shyni"
-  }
+Name = "Shyni-Server"
 }
+}
+
